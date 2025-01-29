bae1a3b5a09825368b3883db3a5e19fb5fc814bb
bon-bon-boon <murakami.ayumu.cn@tut.jp>
2025-01-29 16:32:56 +0900

    "課題3"

--- calc_mul.py
+++ calc_mul.py
@@ -6,10 +6,10 @@ def calc(A,B):
         ai=str(A)
         bi=str(B)
         p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
+        if p.fullmatch(ai) and p.fullmatch(bi):
+                a = float(ai)
+                b = float(bi)
+                if a.is_integer() and b.is_integer() and 1 <= a <= 999 and 1 <= b <= 999:
                         valid=True
                 else:
                         valid=False
